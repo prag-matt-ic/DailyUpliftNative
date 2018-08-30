94bb5849e5c632c589b94a924afce5284617ac36
theFrawley <mattfrawley1@gmail.com>
2018-08-30 17:39:21 +0100

    Pre-removal of TabBar and Stack Nav

--- App.js
+++ App.js
@@ -7,68 +7,63 @@ import Quotes from './app/containers/quotes/Quotes';
 import Container from './app/components/container/Container';
 import Question from './app/components/question/Question';
 
+// const UpliftStack = createStackNavigator(
+//   {
+//     Quotes: Quotes,
+//     Question: Question,
+//   },
+//   {
+//     initialRouteName: 'Quotes',
+//     headerMode: 'none',
+//     navigationOptions: {
+//     // headerStyle: {
+//     // backgroundColor: 'rgba(0,0,0,0.2)',
+//     // },
+//     // headerTintColor: '#fff',
+//     // headerTitleStyle: {
+//     //   fontWeight: 'bold',
+//     // },
+//   }
+//   }
+// );
 
-const UpliftStack = createStackNavigator(
-  {
-    Quotes: Quotes,
-    Question: Question,
-  },
-  {
-    initialRouteName: 'Quotes',
-    headerMode: 'none',
-    navigationOptions: {
-    // headerStyle: {
-    // backgroundColor: 'rgba(0,0,0,0.2)',
-    // },
-    // headerTintColor: '#fff',
-    // headerTitleStyle: {
-    //   fontWeight: 'bold',
-    // },
-  }
-  }
-);
+// const SettingsStack = createStackNavigator(
+//   {
+//     Default: Quotes
+//   },
+//   {
+//     initialRouteName: 'Default',
+//     navigationOptions: {
+//     headerStyle: {
+//     backgroundColor: 'rgba(0,0,0,0.2)',
+//     },
+//     headerTintColor: '#fff',
+//     headerTitleStyle: {
+//       fontWeight: 'bold',
+//     },
+//   }
+//   }
+// );
 
-const SettingsStack = createStackNavigator(
-  {
-    Default: Quotes
-  },
-  {
-    initialRouteName: 'Default',
+// const TabBar = createBottomTabNavigator(
 
-    navigationOptions: {
-    headerStyle: {
-    backgroundColor: 'rgba(0,0,0,0.2)',
-    },
-    headerTintColor: '#fff',
-    headerTitleStyle: {
-      fontWeight: 'bold',
-    },
-  }
-  }
-);
-
-const TabBar = createBottomTabNavigator(
-
-  {
-   Uplift: UpliftStack,
-   Settings: SettingsStack
-  },
-  
-  {
-    initialRouteName: 'Uplift',
-
-    tabBarOptions: {
-      activeTintColor: '#e91e63',
-      labelStyle: {
-        fontSize: 12,
-      },
-      style: {
-        backgroundColor: 'blue',
-      },
-  }
-}
-
-);
+//   {
+//    Uplift: UpliftStack,
+//    Settings: SettingsStack
+//   },
+//   {
+//     initialRouteName: 'Uplift',
+//     tabBarOptions: {
+//       activeTintColor: '#e91e63',
+//       labelStyle: {
+//         fontSize: 12,
+//       },
+//       style: {
+//         backgroundColor: 'blue',
+//       },
+//   }
+// }
+// );
 
 
 
@@ -76,9 +71,10 @@ export default class App extends Component {
 
 render() {
     return (
+      // <Container>
+        <TabBar />
+      // </Container>
   
-            <TabBar />
-       
     );
   }
 }
